37e6d115d59ad24b9fb9df0f87f6e2f2a27a21fb
AntonTrollback <anton@trollback.se>
2021-12-03 15:01:14 +0100

    Update deadline date

--- views/home/index.js
+++ views/home/index.js
@@ -3,7 +3,7 @@ var { i18n } = require('../../components/base')
 var Intro = require('../../components/intro')
 var Countdown = require('../../components/countdown')
 
-var DEADLINE = new Date('2022-06-24')
+var DEADLINE = new Date('2022-05-16')
 
 var text = i18n()
 
