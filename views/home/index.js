a5ac0f201a0363a43a88373acb792ede2ecbae8c
AntonTrollback <anton@trollback.se>
2022-01-19 12:06:14 +0100

    Add GA and link to beckmans

--- views/home/index.js
+++ views/home/index.js
@@ -22,7 +22,7 @@ function home (state, emit) {
         <div class="Home-main">
           <div class="Home-content">
             <p>Din framtid kan börja på Beckmans Kvällsskola!</p>
-            <p>Ett stipendium till för dig som vill gå någon av de tre kvällskurserna på Beckmans Kvällsskola. Familjen Robert Weils stiftelse har grundat Designstipendiet för att alla, oavsett bakgrund, ska kunna ha möjlighet att tacka ja om de erbjuds en plats.</p>
+            <p>Ett stipendium till för dig som vill gå någon av de tre kvällskurserna på <a href="https://beckmans.se/kvallsskolan/" target="_blank">Beckmans Kvällsskola</a>. Familjen Robert Weils stiftelse har grundat Designstipendiet för att alla, oavsett bakgrund, ska kunna ha möjlighet att tacka ja om de erbjuds en plats.</p>
             <nav class="Home-nav">
               ${DEADLINE > Date.now() ? html`<a href="/ansok" class="Home-apply">Sök Stipendiet</a>` : null}
               <a href="/info" class="Home-menu">Mer info</a>
