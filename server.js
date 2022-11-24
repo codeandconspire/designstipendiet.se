69c9f18981f4b254474d59f7e54bc44a5bf1a53a
Anton Trollbäck <anton@trollback.se>
2022-11-24 11:32:42 +0100

    Update live check

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-if (!process.env.HEROKU) require('dotenv/config')
+if (!process.env.FLY) require('dotenv/config')
 
 var got = require('got')
 var jalla = require('jalla')
@@ -10,48 +10,56 @@ var unparsed = require('koa-body/unparsed')
 var purge = require('./lib/purge')
 var email = require('./lib/email')
 
-var ENDPOINT = 'https://docs.google.com/forms/d/e/1FAIpQLSdM-T0zn8tIhIy4s4O9D61mGqaezMUvg2Io-dwkWLQe9dKvbg/formResponse'
+var ENDPOINT =
+  'https://docs.google.com/forms/d/e/1FAIpQLSdM-T0zn8tIhIy4s4O9D61mGqaezMUvg2Io-dwkWLQe9dKvbg/formResponse'
 var app = jalla('index.js', {
   sw: 'sw.js',
-  serve: Boolean(process.env.HEROKU)
+  serve: Boolean(process.env.FLY)
 })
 
 // proxy application form for Google Forms
-app.use(post('/ansok', compose([
-  body({ includeUnparsed: true }),
-  async function (ctx, next) {
-    await got(ENDPOINT, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/x-www-form-urlencoded'
-      },
-      body: ctx.request.body[unparsed]
-    })
+app.use(
+  post(
+    '/ansok',
+    compose([
+      body({ includeUnparsed: true }),
+      async function (ctx, next) {
+        await got(ENDPOINT, {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/x-www-form-urlencoded'
+          },
+          body: ctx.request.body[unparsed]
+        })
 
-    console.log(ctx.request.body[unparsed])
+        console.log(ctx.request.body[unparsed])
 
-    var fields = ctx.request.body.entry
+        var fields = ctx.request.body.entry
 
-    // send an confirmation email
-    await email(fields).catch(console.error)
+        // send an confirmation email
+        await email(fields).catch(console.error)
 
-    if (ctx.accepts('html')) {
-      ctx.redirect('/tack?contact=' + fields['1183121357'])
-    } else {
-      ctx.type = 'application/json'
-      ctx.body = {}
-    }
-  }
-])))
+        if (ctx.accepts('html')) {
+          ctx.redirect('/tack?contact=' + fields['1183121357'])
+        } else {
+          ctx.type = 'application/json'
+          ctx.body = {}
+        }
+      }
+    ])
+  )
+)
 
 // disallow robots anywhere but live
-app.use(get('/robots.txt', function (ctx, next) {
-  ctx.type = 'text/plain'
-  ctx.body = dedent`
+app.use(
+  get('/robots.txt', function (ctx, next) {
+    ctx.type = 'text/plain'
+    ctx.body = dedent`
     User-agent: *
     Disallow: ${app.env === 'production' ? '' : '/'}
   `
-}))
+  })
+)
 
 // set headers
 app.use(function (ctx, next) {
@@ -65,7 +73,7 @@ app.use(function (ctx, next) {
 })
 
 app.listen(process.env.PORT || 8080, function () {
-  if (process.env.HEROKU && app.env === 'production') {
+  if (process.env.FLY && app.env === 'production') {
     purge(['/sw.js'], function (err) {
       if (err) app.emit('error', err)
     })
